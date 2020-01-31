4431ef1c22a14d6c2fa1a09d9189694409e785d6
Aruna <Admin@192.168.0.113>
2020-01-31 15:40:59 +0530

    Aruna ommit

--- attendance-customer-service/src/main/java/com/lng/attendancecustomerservice/serviceImpl/masters/EmpLeaveApproveOrCancelServiceImpl.java
+++ attendance-customer-service/src/main/java/com/lng/attendancecustomerservice/serviceImpl/masters/EmpLeaveApproveOrCancelServiceImpl.java
@@ -60,6 +60,7 @@ public class EmpLeaveApproveOrCancelServiceImpl implements EmpLeaveApproveOrCanc
 							empLeaveDto.setEmpLeaveDaysCount(Integer.valueOf(p[9].toString()));
 							empLeaveDto.setEmpLeaveStatus(p[10].toString());
 							empLeaveDto.setEmpLeaveRemarks(p[11].toString());
+							empLeaveDto.setLeaveType(p[12].toString());
 							empLeaveDtoList.add(empLeaveDto);
 							empLeaveResponseDto.setEmpLeaveDtoList(empLeaveDtoList);
 							empLeaveResponseDto.status = new Status(false, 200, "Success");
